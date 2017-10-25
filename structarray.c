7a69b866d38e8af57766a8977f9a2d37447bc620
Cara Hill <cara@rabidtech.co.nz>
2017-10-25 16:52:20 +1300

    Simple first version of the second struct assignment.

---
+++ structarray.c
@@ -0,0 +1,31 @@
+/* Make an array of structs to hold peoples name and ages, fill array and iterate across it print out the names */
+
+#include <string.h>
+#include <stdio.h>
+
+typedef struct person {
+  char *name;
+  int age;
+} person;
+
+int main ()
+{
+  struct person people[5];
+
+  people[0].name = "Polly McMolly";
+  people[1].name = "George Foreman";
+  people[2].name = "Uma Thurman";
+  people[3].name = "James Dean";
+  people[4].name = "Hermione Granger";
+  people[5].name = "Harry Potter";
+
+  int index = 0;
+
+  while(index < 6)
+  {
+    printf("Person #%d in the array is called %s.\n", index + 1, people[index].name);
+    index++;
+  }
+
+  return 0;
+}
