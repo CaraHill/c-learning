165d8f6b6bd4335bc34fc23f44be769946c9dd81
Cara Hill <cara@rabidtech.co.nz>
2017-10-31 10:23:18 +1300

    Fixed up the struct array programme so you pass in a number arugment, it creates an array of people of that length, randomly assigns names to each person and prints each one out.

--- structarray.c
+++ structarray.c
@@ -9,26 +9,37 @@ typedef struct person {
   int age;
 } person;
 
-char *namearray[10];
-
-namearray[0] = "Polly McMolly";
-namearray[1] = "George Foreman";
-namearray[2] = "Uma Thurman";
-namearray[3] = "James Dean";
-namearray[4] = "Hermione Granger";
-namearray[5] = "Harry Potter";
-namearray[6] = "James Bond";
-namearray[7] = "Elizabeth Bathory";
-namearray[8] = "Tina Turner";
-namearray[9] = "Beyonce";
-
-int main (int argc, int argv)
+int main (int argc, char **argv)
 {
-  struct person people[argv];
+  // allocate space for 10 pointers to strings
+  char **namearray = (char**)malloc(10*sizeof(char*));
+  int i = 0;
+  //allocate space for each string
+  // here allocate 50 bytes, which is more than enough for the strings
+  for(i = 0; i < 10; i++){
+    namearray[i] = (char*)malloc(50*sizeof(char));
+  }
+  //assign them all something
+  sprintf(namearray[0], "Polly McMolly");
+  sprintf(namearray[1], "George Foreman");
+  sprintf(namearray[2], "Uma Thurman");
+  sprintf(namearray[3], "James Dean");
+  sprintf(namearray[4], "Hermione Granger");
+  sprintf(namearray[5], "Harry Potter");
+  sprintf(namearray[6], "Beyonce");
+  sprintf(namearray[7], "James Bond");
+  sprintf(namearray[8], "Elizabeth Bathory");
+  sprintf(namearray[9], "Tina Turner");
+
+  // convert the argument from a string to an integer
+  int arraylength = atoi(argv[1]);
+
+  // create an array of the person struct
+  struct person people[arraylength];
 
   int index = 0;
 
-  while(index < 6)
+  while(index < arraylength)
   {
     people[index].name = namearray[rand() % 10];
     printf("Person #%d in the array is called %s.\n", index + 1, people[index].name);
@@ -37,5 +48,3 @@ int main (int argc, int argv)
 
   return 0;
 }
-
-/* Update by making the size of the array dynamic, and names are assigned to structs within the people array randomly from an array of strings. */
